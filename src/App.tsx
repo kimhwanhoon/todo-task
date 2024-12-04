9b219e99c264086fb05e728af3256de7e0d60dfc
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-12-04 11:43:43 +0100

    input component being made

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+import { TextInput } from './components/input/textInput'
+import { ChevronRight } from './assets/ChevronRight'
+
 function App() {
+  const [todo, setTodo] = useState('')
+  const [inputValue, setInputValue] = useState('')
+
   const handleAddTodo = async () => {
     await fetch('http://localhost:3001/todo', {
       method: 'POST',
@@ -7,6 +14,17 @@ function App() {
   }
   return (
     <main>
+      <TextInput
+        value={inputValue}
+        onChange={setInputValue}
+        classNames={{ container: 'max-w-[285px]' }}
+        rightSection={
+          <ChevronRight
+            size={20}
+            className="cursor-pointer hover:scale-110 duration-300 ease-in-out"
+          />
+        }
+      />
       <button className="p-2 bg-blue-500 text-white" onClick={handleAddTodo}>
         Add Todo
       </button>
--- src/assets/ChevronRight.tsx
+++ src/assets/ChevronRight.tsx
@@ -1,9 +1,16 @@
 interface Props {
-  size: number
-  stroke: number
+  size?: number
+  stroke?: number
+  color?: string
+  className?: string
 }
 
-export const ChevronRight = ({ size, stroke = 2 }: Props) => {
+export const ChevronRight = ({
+  size = 16,
+  stroke = 2,
+  color = '#ABB5BE',
+  className,
+}: Props) => {
   return (
     <svg
       width={size}
@@ -11,11 +18,11 @@ export const ChevronRight = ({ size, stroke = 2 }: Props) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       strokeWidth={stroke}
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
+      className={className}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M9 6l6 6l-6 6" />
